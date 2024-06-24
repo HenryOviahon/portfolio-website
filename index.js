a7cc15b4b64314ee3d276db9b95e41b2f4719402
ijhukhg <henryoviahon@outlook.com>
2024-06-23 19:49:19 -0600

    worked more on button and added extra break to text

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function myFunction() {
 
     var list1 = document.getElementById("list1");
 
+
     if (list1.style.visibility = "hidden"){ //use if statements to determine whether to make visible or hidden
         list1.style.transition = "all, 0.2s";
         list1.style.visibility ="visible";
         list1.style.opacity ="1";
-    }
-    if (list1.style.visibility = "visible"){
+    } else{
         list1.style.transition = "all, 0.2s";
         list1.style.visibility ="hidden";
         list1.style.opacity ="0";
