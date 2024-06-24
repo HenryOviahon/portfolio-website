8cb70b009ed165d5538c0bd7aabba337a3a4d68c
ijhukhg <henryoviahon@outlook.com>
2024-06-23 20:15:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 function myFunction() {
 
     var list1 = document.getElementById("list1");
+    var button1 = document.getElementById("button1");
 
 
     if (list1.style.visibility = "hidden"){ //use if statements to determine whether to make visible or hidden
         list1.style.transition = "all, 0.2s";
         list1.style.visibility ="visible";
         list1.style.opacity ="1";
+        button1.style.background = "blue";
+
     } else if (list1.style.opacity = "1"){
 
         alert("hi")
