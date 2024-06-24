1d23eafd89fbef1327932dc34cb35b7296dc029f
ijhukhg <henryoviahon@outlook.com>
2024-06-23 19:45:37 -0600

    made button work

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
 
-//enable opacity of java items
+//enable opacity of java list
 function myFunction() {
-    var button1 =  document.getElementById("button1");
+
     var list1 = document.getElementById("list1");
 
-    list1.style.visibility = true;
-    list1.style.opacity = 1;
+    if (list1.style.visibility = "hidden"){ //use if statements to determine whether to make visible or hidden
+        list1.style.transition = "all, 0.2s";
+        list1.style.visibility ="visible";
+        list1.style.opacity ="1";
+    }
+    if (list1.style.visibility = "visible"){
+        list1.style.transition = "all, 0.2s";
+        list1.style.visibility ="hidden";
+        list1.style.opacity ="0";
+    }
+
+    
+    
+
 }
     
