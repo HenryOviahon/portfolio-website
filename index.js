3247a97dc88eb727ffb862972f37ca4e84e44f12
ijhukhg <henryoviahon@outlook.com>
2024-07-03 20:24:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,23 @@ function buttonResume() {
     var button1 = document.getElementById("button1");
 
 
-    if (list1.style.visibility == "hidden"){ //use if statements to determine whether to make visible or hidden
-        list1.style.transition = "all, 0.2s";
-        list1.style.visibility ="visible";
-        list1.style.opacity ="1";
-        button1.style.background = "blue";
-
-    } 
-    else {
+    if (list1.style.visibility == "visible"){ //use if statements to determine whether to make visible or hidden
 
         list1.style.transition = "all, 0.2s";
         list1.style.visibility = "hidden";
         list1.style.opacity ="0";
         button1.style.background = "rgb(9, 140, 255)";
+
+        
+
+    } 
+    else {
+
+        list1.style.transition = "all, 0.2s";
+        list1.style.visibility ="visible";
+        list1.style.opacity ="1";
+        button1.style.background = "blue";
+        
     }
 
     
